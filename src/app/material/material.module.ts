0b01ac8d6181a2428885a866bcb17260af09917b
Subramanya K S <subramanyaks2205@gmail.com>
2023-08-01 22:39:31 +0530

    updated material module

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -3,11 +3,15 @@ import { MatButtonModule } from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatCardModule} from '@angular/material/card';
 
 const MaterialComponent = [
   MatButtonModule,
   MatIconModule,
   MatToolbarModule,
+  MatCardModule,
+  MatDividerModule,
   MatSlideToggleModule
 ];
 
