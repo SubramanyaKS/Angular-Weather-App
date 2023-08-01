ec4b625aeaf0589bfcb929381f781a8c63840e63
Subramanya K S <subramanyaks2205@gmail.com>
2023-08-01 22:37:14 +0530

    updated app service

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../environments/environment.weather';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  public data:string="";
+  private apiKey = environment.apiKey;
+  private apiUrl = environment.apiUrl;
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  fetchData(city:string):Observable<any>{
+    return this.http.get(`${this.apiUrl}?q=${city}&units=metric&appid=${this.apiKey}`)
+  }
 }
