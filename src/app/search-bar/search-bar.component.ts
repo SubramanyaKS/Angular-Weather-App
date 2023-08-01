5d339352e2fb249275022ddc2c6557e0ee427d9e
Subramanya K S <subramanyaks2205@gmail.com>
2023-08-01 22:38:02 +0530

    created and updated search bar

---
+++ src/app/search-bar/search-bar.component.ts
@@ -0,0 +1,35 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { ApiService } from '../api.service';
+
+@Component({
+  selector: 'app-search-bar',
+  templateUrl: './search-bar.component.html',
+  styleUrls: ['./search-bar.component.css']
+})
+export class SearchBarComponent implements OnInit {
+  location:string="";
+  @Output() weatherDataEvent = new EventEmitter<any>();
+
+  constructor(private apiService:ApiService) { }
+
+  ngOnInit(): void {
+  }
+  search(){
+    if(this.location){
+      this.apiService.fetchData(this.location).subscribe({
+        next :(res)=>{
+          console.log(res);
+          this.weatherDataEvent.emit(res);
+        },
+        error:(err)=>{
+          console.error(err);
+        },
+        complete:()=>{
+          console.log("All Test Completed")
+        }
+      }
+      );
+    }
+  }
+
+}
