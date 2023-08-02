5780643382ed64482335804a0f235d099597172f
Subramanya K S <subramanyaks2205@gmail.com>
2023-08-02 22:44:12 +0530

    updated icons

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -10,10 +10,15 @@ export class ApiService {
   public data:string="";
   private apiKey = environment.apiKey;
   private apiUrl = environment.apiUrl;
+  private iconUrl = environment.iconUrl;
 
   constructor(private http:HttpClient) { }
 
   fetchData(city:string):Observable<any>{
     return this.http.get(`${this.apiUrl}?q=${city}&units=metric&appid=${this.apiKey}`)
+  }
+  getWeatherIconUrl(iconCode: string): string {
+    // console.log(iconCode);
+    return `${this.iconUrl}/${iconCode}.png`;
   }
 }
