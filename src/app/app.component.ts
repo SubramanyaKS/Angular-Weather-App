9964778aef5caaae4019a828f085b920ee87cdfa
SubramanyaKS <subramanyaks2205@gmail.com>
2026-08-22 17:23:33 +0530

    Bootstrap using standalone APIs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { ApiService } from './api.service';
+import { HeaderComponent } from './header/header.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { CardComponent } from './card/card.component';
+import { FooterComponent } from './footer/footer.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
-    standalone: false
+    imports: [HeaderComponent, SearchBarComponent, CardComponent, FooterComponent, RouterOutlet]
 })
 export class AppComponent {
   weatherData: any;
